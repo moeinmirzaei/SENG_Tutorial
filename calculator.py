cfcc1c606ba5bc371c481890eca31b69e2a772f3
Moein <moein.mirzaei@toptal.com>
2023-10-10 14:23:38 -0600

    Implement Calculator class

---
+++ calculator.py
@@ -0,0 +1,8 @@
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def minus(a, b):
+        return a - b
---
+++ main.py
@@ -0,0 +1,4 @@
+from calculator import Calculator
+
+print(Calculator.add(2, 3))
+print(Calculator.minus(6, 3))
